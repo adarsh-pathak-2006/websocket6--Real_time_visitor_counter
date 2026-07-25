b63de16396d6e69a20ece84a5992f9611b5cbd3c
duckyduck01 <madhvetipathak2006@gmail.com>
2026-07-25 17:32:58 +0530

    added templates  and wired them through settings

--- counter/settings.py
+++ counter/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework', 'core',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'counter.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
