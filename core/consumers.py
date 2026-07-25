b45690ff7a7a628efb9b6c7b99c86618a0d39d77
duckyduck01 <madhvetipathak2006@gmail.com>
2026-07-25 18:36:58 +0530

    made the error visible on the screen

--- core/consumers.py
+++ core/consumers.py
@@ -4,19 +4,23 @@ import json
 class CounterConsumer(AsyncWebsocketConsumer):
     user_counter=0
     async def connect(self):
-        self.user=self.scope["user"]
-        if self.user.is_authenticated:
-            self.group_name="group"
-            await self.channel_layer.group_add(self.group_name, self.channel_name)
-            await self.accept()
-            CounterConsumer.user_counter=CounterConsumer.user_counter+1
-        else:
+        await self.accept()
+        try:
+            self.user=self.scope["user"]
+            if self.user.is_authenticated:
+                self.group_name="group"
+                await self.channel_layer.group_add(self.group_name, self.channel_name)
+                CounterConsumer.user_counter=CounterConsumer.user_counter+1
+                await self.channel_layer.group_send(self.group_name, {
+                    'type':'user_count',
+                    'count':CounterConsumer.user_counter,    
+                })
+            else:
+                await self.send(text_data=json.dumps({'error': 'User is not authenticated'}))
+                await self.close()
+        except Exception as e:
+            await self.send(text_data=json.dumps({'error': str(e)}))
             await self.close()
-            return
-        await self.channel_layer.group_send(self.group_name, {
-            'type':'user_count',
-            'count':CounterConsumer.user_counter,    
-        })
 
     async def user_count(self, event):
         await self.send(text_data=json.dumps({ 'count': event["count"] }))
