c5c9d4406d4bb754d95e6d24f595c076c15ffab1
duckyduck01 <madhvetipathak2006@gmail.com>
2026-07-25 18:51:53 +0530

    fixed the counter not decreasing error

--- core/consumers.py
+++ core/consumers.py
@@ -29,9 +29,10 @@ class CounterConsumer(AsyncWebsocketConsumer):
         if not hasattr(self, "group_name"):
             return
         CounterConsumer.user_counter=CounterConsumer.user_counter-1
-        await self.channel_layer.group_discard(self.group_name, self.channel_name)
         await self.channel_layer.group_send(self.group_name, {
             'type':'user_count',
             'count':CounterConsumer.user_counter,    
         })
+        await self.channel_layer.group_discard(self.group_name, self.channel_name)
+        
         
